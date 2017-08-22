0540317c3396f7cb94460ef6e7b1152eea2671c6
Oscar Alcantara <oukiar@gmail.com>
2017-08-22 18:58:22 -0500

    Deploy in progress

--- deploy.py
+++ deploy.py
@@ -13,7 +13,7 @@ os.system('git push')
 if len(sys.argv) > 1: 
     if sys.argv[1] == "release":
         print("Deploying on the Main server")
-        os.system('ssh nat@nylas.orgboat.com "cd sync_engine; git pull"')
+        os.system('ssh nat@nylas.orgboat.com "cd sync-engine; git pull"')
     else:
         print("Unkwnown option")
 else:
