44a1b8dd7062ad66e8c7aad6db3cbd67cd50a8ce
Oscar Alcantara <oukiar@gmail.com>
2017-08-22 18:57:48 -0500

    Deploy in progress

---
+++ deploy.py
@@ -0,0 +1,21 @@
+'''
+This script will upload all changes to the main server repo and update the production directory ...
+'''
+
+import os
+import sys
+
+
+os.system('git add --all .')
+os.system('git commit -m "Deploy in progress"')
+os.system('git push')
+
+if len(sys.argv) > 1: 
+    if sys.argv[1] == "release":
+        print("Deploying on the Main server")
+        os.system('ssh nat@nylas.orgboat.com "cd sync_engine; git pull"')
+    else:
+        print("Unkwnown option")
+else:
+    print("Git push done!")
+    
