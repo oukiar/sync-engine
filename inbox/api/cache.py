4ded7cf6c6c1138c725ed9839331f5a3357a285a
Oscar Alcantara <oukiar@gmail.com>
2017-08-24 01:34:23 -0500

    Deploy in progress

--- inbox/api/cache.py
+++ inbox/api/cache.py
@@ -1,19 +1,20 @@
 from flask.ext.cache import Cache    
 cache = Cache()
+import json
 
 cache_ram = {}
 
 def get_cached(data):
-    hashval = hash(frozenset( data ))
+    hashval = json.dumps( data )
     print('GETING: ', hashval)
     
     if hashval in cache_ram:
-        print(cache_ram[hashval])
+        print('CACHE_RAM', cache_ram[hashval])
         return cache_ram[hashval]
     else:
         return None
     
 def set_cached(data, result):
-    hashval = hash(frozenset( data ))
+    hashval = json.dumps( data )
     cache_ram[hashval] = result
     
