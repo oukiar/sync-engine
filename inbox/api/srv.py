e5cae9c7a4525092d0f00e1904218d83b1fd0c6e
Oscar Alcantara <oukiar@gmail.com>
2017-09-09 20:53:51 -0500

    Deploy in progress

--- inbox/api/srv.py
+++ inbox/api/srv.py
@@ -37,9 +37,12 @@ from threading import Timer
 def sync_timeout():
     print 'Executing sync account mailboxes sync timeout'
     
+    for i in  mailboxes_timeouts:
+        if time.time() - mailboxes_timeouts[i] > 20:
+            del mailboxes_timeouts[i]
     
 # duration is in seconds
-t = Timer(30, sync_timeout)
+t = Timer(10, sync_timeout)
 t.start()
 
 def default_json_error(ex):
