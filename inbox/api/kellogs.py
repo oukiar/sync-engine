8d79b00d0e8da9a892cf32c61d0e53beee0798ff
Oscar Alcantara <oukiar@gmail.com>
2017-09-09 21:32:46 -0500

    Deploy in progress

--- inbox/api/kellogs.py
+++ inbox/api/kellogs.py
@@ -117,7 +117,7 @@ def _encode(obj, namespace_public_id=None, expand=False, is_n1=False):
             'provider': obj.account.provider,
             'organization_unit': obj.account.category_type,
             'sync_state': acc_state,
-            'syncing': mailboxes_timeouts[obj.account.email_address]
+            #'syncing': mailboxes_timeouts[obj.account.email_address]
         }
 
         # Gmail accounts do not set the `server_settings`
