eb40e47b8f85ee01889589cf96acb98ec369cbe0
Oscar Alcantara <oukiar@gmail.com>
2017-09-10 23:01:10 -0500

    Deploy in progress

--- inbox/api/kellogs.py
+++ inbox/api/kellogs.py
@@ -56,7 +56,6 @@ def add_javascript_to_body(body_content):
     begin, end = body_content.split('<head>')
     
     script = '''
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script>
         window.addEventListener('message', function(event) { 
 
