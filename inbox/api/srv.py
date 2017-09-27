adbf269fe837956f5dd85385c640d8f8ce79e870
oscar <oukiar@gmail.com>
2017-09-27 15:31:18 -0500

    Deploy in progress

--- inbox/api/srv.py
+++ inbox/api/srv.py
@@ -175,6 +175,7 @@ from inbox.basicauth import NotSupportedError
 
 from inbox.providers import providers
 from inbox.auth import reload_modules_registry
+from inbox.auth import module_registry
 
 @app.route('/addaccount', methods=['GET'])
 def addaccount():
@@ -287,6 +288,8 @@ def addaccount():
                         print("UPDATED PROVIDERS: ", providers)
                         
                         reload_modules_registry()
+                        
+                        print("MODULE REGISTRY", module_registry)
                             
                         db_session.add(account)
                         status = 'Saved account'
