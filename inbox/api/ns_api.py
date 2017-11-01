79fbb7111926cc720cee6f09f9765d6f4b0aab26
Oscar Alcantara <oukiar@gmail.com>
2017-11-01 17:25:29 -0600

    Deploy in progress

--- inbox/api/ns_api.py
+++ inbox/api/ns_api.py
@@ -526,10 +526,10 @@ def message_query_api():
                         public_id = i.get("src").split(':')[1]
                         print("CONTENT_PUBLIC_ID: ", public_id)
                         
-                        for i in msg.api_attachment_metadata:
-                            print i
-                            if i['content_id'] == public_id:
-                                public_id = i['id']
+                        for j in msg.api_attachment_metadata:
+                            print j
+                            if j['content_id'] == public_id:
+                                public_id = j['id']
                                 break
                                 
                         print("PUBLIC_ID: ", public_id)
