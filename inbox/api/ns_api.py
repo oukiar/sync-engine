40e4485f780bd16d9364b832c0ec4829a0dda2db
Oscar Alcantara <oukiar@gmail.com>
2017-11-16 13:10:15 -0600

    Deploy in progress

--- inbox/api/ns_api.py
+++ inbox/api/ns_api.py
@@ -552,7 +552,7 @@ def message_query_api():
     g.parser.add_argument('unread', type=strict_bool, location='args')
     g.parser.add_argument('starred', type=strict_bool, location='args')
     g.parser.add_argument('view', type=view, location='args')
-    g.parser.add_argument('withoutbody', type=bounded_str, location='args')
+    g.parser.add_argument('withoutbody', type=strict_bool, location='args')
 
     args = strict_parse_args(g.parser, request.args)
 
@@ -597,7 +597,7 @@ def message_query_api():
 
         #fix for sanitize the body
         for msg in messages:
-            if args['withoutbody'] != "YES":
+            if args['withoutbody'] != True:
                     
                 print("+++++++++++ SUBJECT: ", msg.subject)
                 msg.bodySanitized = sanitize(msg)
